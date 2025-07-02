3aa8cbc7508e3d701e21abca0fc2da82c6efa849
Vidula Deneth Salwathura <viduladenethsalwathura@gmail.com>
2025-07-02 21:33:57 +0530

    added profile tab section

---
+++ app/(tabs)/profile/EditProfile.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native";
+
+export default function EditProfile() {
+    return(
+        <View>
+            <Text>
+                Edit Profile
+            </Text>
+        </View>
+    )
+}
